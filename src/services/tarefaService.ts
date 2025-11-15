13147af81d29f3cf957682b109b38cfe1ca72eda
orlando-IDA <orlandogoncalves8585@gmail.com>
2025-11-15 19:46:43 -0300

    refactor: adicional parametro de sinal para a tarefaService.

--- src/services/tarefaService.ts
+++ src/services/tarefaService.ts
@@ -6,7 +6,6 @@ import type {
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-
 async function handleJsonResponse(response: Response) {
   if (!response.ok) {
     let errorData;
@@ -38,38 +37,43 @@ async function handleEmptyResponse(response: Response) {
   return; 
 }
 
-export async function getTarefasPorUsuario(idUsuario: number): Promise<ITarefa[]> {
+export async function getTarefasPorUsuario(idUsuario: number, signal?: AbortSignal): Promise<ITarefa[]> {
   const response = await fetch(`${API_URL}/tarefas/usuario/${idUsuario}`, {
     method: 'GET',
+    signal: signal,
   });
-  return handleJsonResponse(response);
+  const data = await handleJsonResponse(response);
+  return data || [];
 }
 
-export async function createTarefa(data: ITarefaCreate): Promise<ITarefa> {
+export async function createTarefa(data: ITarefaCreate, signal?: AbortSignal): Promise<ITarefa> {
   const response = await fetch(`${API_URL}/tarefas`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
+    signal: signal,
   });
   return handleJsonResponse(response);
 }
 
-export async function updateTarefa(idTarefa: number, data: ITarefaUpdate): Promise<ITarefa> {
+export async function updateTarefa(idTarefa: number, data: ITarefaUpdate, signal?: AbortSignal): Promise<ITarefa> {
   const response = await fetch(`${API_URL}/tarefas/${idTarefa}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
+    signal: signal,
   });
   return handleJsonResponse(response);
 }
 
-export async function deleteTarefa(idTarefa: number): Promise<void> {
+export async function deleteTarefa(idTarefa: number, signal?: AbortSignal): Promise<void> {
   const response = await fetch(`${API_URL}/tarefas/${idTarefa}`, {
     method: 'DELETE',
+    signal: signal,
   });
   return handleEmptyResponse(response);
 }
